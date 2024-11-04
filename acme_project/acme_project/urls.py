abe9260826dc5e037b7bdde3e0eda60d088bd7ba
natallimir <natallimir82@mail.ru>
2024-11-04 11:56:44 +0500

    end auth

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 
 urlpatterns = [
@@ -9,4 +11,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    )
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
